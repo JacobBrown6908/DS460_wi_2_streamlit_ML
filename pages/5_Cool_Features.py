7061dd156d80989d9dbf42da31a04e67184ef312
JacobBrown <jcb99mt@byui.edu>
2025-03-24 21:52:51 -0600

    headers and names change and page order changes

--- pages/5_Cool_Features.py
+++ pages/5_Cool_Features.py
@@ -6,6 +6,8 @@ st.write("#### Below are some cool features that Streamlit offers.")
 
 st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
+st.subheader("Fundamentals") 
+
 st.link_button("Click me!", "https://docs.streamlit.io/get-started/fundamentals/additional-features")
 
 st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
--- streamlit_app.py
+++ streamlit_app.py
@@ -2,16 +2,16 @@ import streamlit as st
 
 st.subheader("Learnig Goals")
 
-st.write("1. Understand how to start a new Streamlit app")
+st.write("#### 1. Understand how to start a new Streamlit app")
 st.write("   ")
-st.write("2. Understand how streamlit conects with Github")
+st.write("#### 2. Understand how streamlit conects with Github")
 st.write("   ")
-st.write("3. Understand baisc and dynamic Streamlit functions")
+st.write("#### 3. Understand baisc and dynamic Streamlit functions")
 st.write("   ")
-st.write("4. Understand how to integating an ML model into streamlit")
+st.write("#### 4. Understand how to integating an ML model into streamlit")
 st.write("   ")
-st.write("5. Understand Cool Features and Components of Streamlit")
+st.write("#### 5. Understand Cool Features and Components of Streamlit")
 st.write("   ")
-st.write("6. Understand Streamlit with Docker")
+st.write("#### 6. Understand Streamlit with Docker")
 
 
