0c31e7ea744804c818552bb0edd1f74d75cb7444
JacobBrown <jcb99mt@byui.edu>
2025-03-24 19:27:17 -0600

    cleaned code refrences

--- pages/1_code.py
+++ pages/1_code.py
@@ -2,34 +2,30 @@ import streamlit as st
 
 st.title("Coding Library")
 
-
 st.write("#### import streamlit as st")
 
-st.write("This imports the streamlit library and allows us to use all of its functions")
+st.write("This imports the Streamlit library and allows us to use all of its functions.")
 
 st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
 st.subheader("String Functions")
 
 st.write("#### st.title('')")
-st.write("This function allows us to add a title to the top of the page")
+st.write("This function allows us to add a title to the top of the page.")
 
 st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
 st.write("#### st.subheader('')")
-st.write("This function allows us to add a subheader to the page")
+st.write("This function allows us to add a subheader to the page.")
 
 st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
 st.write("#### st.write('')")
-st.write("   ")
-
 st.write("This is the most basic function that allows us to write text to the page. Also, using `##` in Markdown allows us to create headers in the `write` function.")
 
 st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
 st.subheader("Interactive Functions")
-
 
 st.write("#### st.button('')")
 st.write("This function creates a button that can be clicked, with a variable that is callable.")
@@ -43,3 +39,4 @@ st.write("This function creates a text input box that can be filled out by the u
 
 fav_color = st.text_input("My name's Buddy. What's your favorite color?")
 st.write(f"{fav_color} is my favorite color too!")
+
