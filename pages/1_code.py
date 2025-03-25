bb8d15747a49871cd233f37ebe4d3b101e3c61bc
JacobBrown <jcb99mt@byui.edu>
2025-03-24 19:18:17 -0600

    new lines across

--- pages/1_code.py
+++ pages/1_code.py
@@ -7,24 +7,24 @@ col1, col2 = st.columns([1, 1])
 col1.write("##### import streamlit as st")
 col2.write("This imports the streamlit library and allows us to use all of its functions")
 
-st.markdown("---")  # Full-width separator
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
 col1.subheader("String Functions")
 
 col1.write("##### st.title('')")
 col2.write("This function allows us to add a title to the top of the page")
 
-st.markdown("---")  # Full-width separator
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)
 
 col1.write("##### st.subheader('')")
 col2.write("This function allows us to add a subheader to the page")
 
-st.markdown("---")  # Full-width separator
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)
 
 col1.write("##### st.write('')")
 col2.write("This is the most basic function that allows us to write text to the page. Also, using `##` in Markdown allows us to create headers in the `write` function.")
 
-st.markdown("---")  # Full-width separator
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)
 
 col1.subheader("Interactive Functions")
 
@@ -33,7 +33,7 @@ col2.write("This function creates a button that can be clicked, with a variable 
 
 is_click = col2.link_button("Click Me", "https://www.youtube.com/shorts/SXHMnicI6Pg")
 
-st.markdown("---")  # Full-width separator
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)
 
 col1.write("##### st.text_input('')")
 col2.write("This function creates a text input box that can be filled out by the user, with a variable that is callable.")
