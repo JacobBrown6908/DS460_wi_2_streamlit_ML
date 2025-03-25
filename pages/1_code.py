c980cca9b6620cba3e534ba2c90052127f722481
JacobBrown <jcb99mt@byui.edu>
2025-03-24 19:17:12 -0600

    lines across

--- pages/1_code.py
+++ pages/1_code.py
@@ -1,51 +1,42 @@
 import streamlit as st
 
-
-st.title("coding library")
-
+st.title("Coding Library")
 
 col1, col2 = st.columns([1, 1])
 
 col1.write("##### import streamlit as st")
 col2.write("This imports the streamlit library and allows us to use all of its functions")
-col1.markdown("---")
-col2.markdown("---")
 
-col1.markdown("  ")
-col2.markdown("  ")
+st.markdown("---")  # Full-width separator
 
 col1.subheader("String Functions")
 
 col1.write("##### st.title('')")
 col2.write("This function allows us to add a title to the top of the page")
-col1.markdown("---")
-col2.markdown("---")
+
+st.markdown("---")  # Full-width separator
 
 col1.write("##### st.subheader('')")
 col2.write("This function allows us to add a subheader to the page")
-col1.markdown("---")
-col2.markdown("---")
+
+st.markdown("---")  # Full-width separator
 
 col1.write("##### st.write('')")
-col2.write("this is the most basic function allows us to write text to the page. Also the Use of ## from the markdown language allows us to create headers in the write function")
+col2.write("This is the most basic function that allows us to write text to the page. Also, using `##` in Markdown allows us to create headers in the `write` function.")
 
-col1.markdown("  ")
-col2.markdown("  ")
+st.markdown("---")  # Full-width separator
+
 col1.subheader("Interactive Functions")
-col1.markdown("  ")
-col2.markdown("  ")
 
 col1.write("##### st.button('')")
-col2.write("This function creates a button that can be clicked, with a varible that callable")
-is_click = col2.link_button("Click Me" ,"https://www.youtube.com/shorts/SXHMnicI6Pg")
-col1.markdown("---")
-col2.markdown("---")
+col2.write("This function creates a button that can be clicked, with a variable that is callable.")
 
+is_click = col2.link_button("Click Me", "https://www.youtube.com/shorts/SXHMnicI6Pg")
+
+st.markdown("---")  # Full-width separator
 
 col1.write("##### st.text_input('')")
-col2.write("This function creates a text input box that can be filled out by the user, with a varible that is callable")
+col2.write("This function creates a text input box that can be filled out by the user, with a variable that is callable.")
 
-fav_color = st.text_input('My names Buddy whats your favorit color?')
+fav_color = st.text_input("My name's Buddy. What's your favorite color?")
 st.write(f"{fav_color} is my favorite color too!")
-
-
