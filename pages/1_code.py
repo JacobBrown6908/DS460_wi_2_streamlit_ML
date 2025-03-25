833c8f2ccc7a60acf52bdff3c46a30c20eefdcf1
JacobBrown <jcb99mt@byui.edu>
2025-03-24 19:25:42 -0600

    no columns

--- pages/1_code.py
+++ pages/1_code.py
@@ -2,51 +2,44 @@ import streamlit as st
 
 st.title("Coding Library")
 
-col1, col2 = st.columns([1, 1])
 
-col1.write("##### import streamlit as st")
-col1.write("   ")
+st.write("#### import streamlit as st")
 
-col2.write("This imports the streamlit library and allows us to use all of its functions")
+st.write("This imports the streamlit library and allows us to use all of its functions")
 
-col1.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
-col2.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
-col1.subheader("String Functions")
+st.subheader("String Functions")
 
-col1.write("##### st.title('')")
-col2.write("This function allows us to add a title to the top of the page")
+st.write("#### st.title('')")
+st.write("This function allows us to add a title to the top of the page")
 
-col1.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
-col2.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
-col1.write("##### st.subheader('')")
-col2.write("This function allows us to add a subheader to the page")
+st.write("#### st.subheader('')")
+st.write("This function allows us to add a subheader to the page")
 
-col1.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
-col2.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
-col1.write("##### st.write('')")
-col1.write("   ")
+st.write("#### st.write('')")
+st.write("   ")
 
-col2.write("This is the most basic function that allows us to write text to the page. Also, using `##` in Markdown allows us to create headers in the `write` function.")
+st.write("This is the most basic function that allows us to write text to the page. Also, using `##` in Markdown allows us to create headers in the `write` function.")
 
-col1.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
-col2.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
-col1.subheader("Interactive Functions")
+st.subheader("Interactive Functions")
 
 
-col1.write("##### st.button('')")
-col2.write("This function creates a button that can be clicked, with a variable that is callable.")
+st.write("#### st.button('')")
+st.write("This function creates a button that can be clicked, with a variable that is callable.")
 
-is_click = col2.link_button("Click Me", "https://www.youtube.com/shorts/SXHMnicI6Pg")
+is_click = st.link_button("Click Me", "https://www.youtube.com/shorts/SXHMnicI6Pg")
 
-col1.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
-col2.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
 
-col1.write("##### st.text_input('')")
-col2.write("This function creates a text input box that can be filled out by the user, with a variable that is callable.")
+st.write("#### st.text_input('')")
+st.write("This function creates a text input box that can be filled out by the user, with a variable that is callable.")
 
 fav_color = st.text_input("My name's Buddy. What's your favorite color?")
 st.write(f"{fav_color} is my favorite color too!")
