44537c297aa39b8a0461c06361f355216d7a67ed
BrandonAnsbergs <ans22004@byui.edu>
2025-03-24 21:37:46 -0600

    Feature page added

---
+++ pages/5_Cool_Features.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.title("Cool Features")
+
+st.write("#### Below are some cool features that Streamlit offers.")
+
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
+
+st.subheader("features.button", "Click me!", "https://docs.streamlit.io/get-started/fundamentals/additional-features")
+
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
+
+st.subheader("Components")
+
+st.button("components.button", "Click me!", "https://streamlit.io/components?category=authentication")
+
+st.markdown('<hr style="border:1px dashed #ccc;">', unsafe_allow_html=True)  # Full-width dashed line
